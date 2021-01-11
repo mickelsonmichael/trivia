2400d7271bb61cd5e56d3e4f3450c7c730d393c4
Michael Mickelson <mickelsonmichael@gmail.com>
2021-01-10 20:24:04 -0700

    Add initial board styles and layout

--- src/board/index.js
+++ src/board/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "./board.css";
+
+const answers = [
+  "Thomas Jefferson",
+  "Benjamin Franklin",
+  "George Washington",
+  "George Bush",
+];
 
 const Board = () => (
-	<div className="board">
-		<div>This is the boaord</div>
-	</div>
+  <div className="board">
+    <div className="board__question">
+      Who was the first president of the United States?
+    </div>
+    <div className="board__answers">
+      {answers.map((a) => (
+        <div className="board__answers__answer">{a}</div>
+      ))}
+    </div>
+  </div>
 );
 
 export default Board;
-
